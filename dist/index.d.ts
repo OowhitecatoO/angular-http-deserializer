df265f6f224404bfae91765cc97930d686e9ae5d
WhiteCat <whitecatx6@gmail.com>
2020-06-16 22:02:56 +0800

    Remove dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import 'reflect-metadata';
-import { newable, inputValue, outputValue, deserializeSig } from './types';
-export default function deserializer<T extends outputValue>(type: newable): deserializeSig<T>;
-export declare function convert<T extends outputValue>(deserializeValue: inputValue, expectedType?: string | newable, valueTypeString?: string): T;
-export declare function deserialize<T extends outputValue>(type: newable, deserializeData: inputValue, mustBeArray?: boolean | number): T | T[];
